0836f5e5a0c0fa57459f3008329d4faef3a80063
User803 <jacksongachugu@gmail.com>
2025-02-08 13:05:35 +0300

    Fixed bugs

--- blog/src/main/java/com/app/blog/domain/dtos/UpdatePostRequestDto.java
+++ blog/src/main/java/com/app/blog/domain/dtos/UpdatePostRequestDto.java
@@ -40,5 +40,5 @@ public class UpdatePostRequestDto {
     private Set<UUID> tagIds = new HashSet<>();
 
     @NotNull(message = "Status is required")
-    private PostStatus postStatus;
+    private PostStatus status;
 }
